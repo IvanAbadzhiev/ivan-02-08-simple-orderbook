a8f4a047d5380384e44e408c0abca5ede8c596bf
Ivan Abadzhiev <abadzhievltd@gmail.com>
2021-08-02 01:04:03 +0300

    moved the enums to the central place

--- src/types/Enums.ts
+++ src/types/Enums.ts
@@ -7,3 +7,13 @@ export enum OrderType {
 	ASK = "ask",
 	BID = "bid"
 }
+
+export enum Product {
+	PI_XBTUSD = "PI_XBTUSD",
+	PI_ETHUSD = "PI_ETHUSD"
+}
+
+export enum WebsocketEventEnum {
+	SUBSCRIBE = "subscribe",
+	UNSUBSCRIBE = "unsubscribe"
+}
